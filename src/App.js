39719c3317f8679cb13eb10e97b186e88a925c1b
tanikawa <kazuya.tanikawa@gmail.com>
2017-02-26 19:13:27 +0900

    add new file

---
+++ src/App.js
@@ -0,0 +1,67 @@
+/* ライブラリのインポート */
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+/* Action Creatorのインポート */
+import { addText, clearText } from './actions'
+
+/*
+  View (Reactコンポーネント):
+  ユーザーの操作などを受けて dispatch() メソッドで Action Creator (./actions.jsからインポートした addText(), clearText()メソッドなど) を呼び出して、Storeにデータの変更 (stateの変更) を伝播させる
+  dispatchの例: this.props.dispatch(clearText())
+  dispatch() メソッドは connect() メソッド (react-reduxよりインポートされており、App.js最下部で使用) でラップすることにより使用可能になる
+*/
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <input type='text' ref='input' /><br/>
+                <button onClick={(e) => this.onAddBtnClicked(e)}   >Add</button>
+                <button onClick={(e) => this.onClearBtnClicked(e)} >Clear</button>
+                <ul>
+                  {
+                    //state中のオブジェクトをループさせて<li>要素を描画。stateは selector() メソッドで指定しているものがpropsとして渡ってくる
+                    this.props.state.storedText.map((obj) =>
+                      <li key={obj.id} >
+                        {obj.text}
+                      </li>
+                    )
+                  }
+                </ul>
+            </div>
+        )
+    }
+
+    //Add ボタンをクリックした時に呼び出される
+    onAddBtnClicked(e) {
+        let input = this.refs.input
+        let text = input.value.trim()
+        if (!text) return alert('何かテキストを入力してください。')
+        input.value = ''
+        // Appコンポーネントが connect() メソッドでラップされていることによって、dispatchメソッドを呼び出すことが可能になる
+        // dispatch() メソッドで ActionCreator である addText() メソッドをラップして呼び出すことによってデータの変更を伝播する
+        this.props.dispatch(addText(text))
+    }
+
+    //Clear ボタンをクリックした時に呼び出される
+    onClearBtnClicked(e) {
+        // dispatchメソッドで ActionCreator であるclearText() メソッドをラップして呼び出すことによってデータの変更を伝播する
+        this.props.dispatch(clearText())
+    }
+
+}
+
+// セレクターの定義: Appコンポーネントが必要とするデータを グローバルなstate 全体の中から取捨選択して取得する。今回は state 全体をそのままreturnしている
+let selector = (state) => {
+    // [storedText]というキー名はreducer.jsの最下部で設定している Store のキー名
+    console.log(state.storedText);
+    return {
+        state: state // Key名とvalue名が同じなので return {state} でも可: Object Literal Shorthand
+    }
+}
+
+// Appコンポーネントを connect() メソッドでラップした上でエクスポート
+// 第一引数には上述のselector() メソッドを使って、stateの中からコンポーネントが必要とするデータを指定、第二引数にはコンポーネント自体を指定。その上でエクスポート。
+export default connect(selector)(App)
---
+++ src/index.js
@@ -0,0 +1,29 @@
+/* ライブラリのインポート */
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider }  from 'react-redux'
+
+/* Appコンポーネントのインポート */
+import App from './App'
+/* Reducerから Store をインポート */
+import { store } from './reducer'
+
+/*
+  Entry Point:
+   - Appコンポーネント (App.jsからインポート) をProviderコンポーネント (react-reduxからインポート) でラップする
+   - さらに、createStore() メソッドで生成した Store をProviderコンポーネントに設定する
+*/
+
+//createStore() メソッドでアプリケーションのStateを保存する Store を生成する。
+let applicationStore = createStore(store);
+let rootElement = document.querySelector('#root');
+
+render (
+    //Providerに作成したstoreをセット
+    <Provider store={applicationStore}>
+        <App />
+    </Provider>,
+    // rootElement下にコンポーネントを生成
+    rootElement
+);
